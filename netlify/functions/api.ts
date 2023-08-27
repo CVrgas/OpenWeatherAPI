ca7ee187b56e9b6223f3e926780a28c162b8b8a2
CVrgas <usual389@gmail.com>
2023-08-27 14:57:24 -0400

    get city from params

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -9,15 +9,17 @@ const APIKey = process.env.APIKey;
 api.use(cors());
 
 const router = Router();
-router.get("/hello", (req, res) =>
+router.get("/hello", (req, res) => res.send("<h1>Hellow<h1>"));
+router.get("/hello/:city", (req, res) => {
+	const city = req.params.city;
 	fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=COLOMBIA}&appid=${APIKey}`
+		`https://api.openweathermap.org/data/2.5/weather?q=${city}}&appid=${APIKey}`
 	)
 		.then((response) => response.json())
 		.then((json) => {
 			res.send(json);
-		})
-);
+		});
+});
 
 api.use("/api/", router);
 
